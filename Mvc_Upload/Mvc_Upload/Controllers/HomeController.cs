b18462a2d5255f5b02bb4a9c4144dfae81b00c62
Jéssica Cruz <jessica.santos648@gmail.com>
2020-09-04 15:05:58 -0300

    Upload de arquivos

---
+++ Mvc_Upload/Mvc_Upload/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Mvc_Upload.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Upload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Index(Remessa arq)
+        {
+            try
+            {
+                string nomeArquivo = "";
+                string arquivoEnviado = "";
+                foreach (var arquivo in arq.Arquivos)
+                {
+                    if(arquivo.ContentLength > 0)
+                    {
+                        nomeArquivo = Path.GetFileName(arquivo.FileName);
+                        var caminho = Path.Combine(Server.MapPath("~/Imagens"), nomeArquivo);
+                        arquivo.SaveAs(caminho);
+                    }
+
+                    arquivoEnviado = arquivoEnviado + "," + nomeArquivo;
+                }
+                ViewBag.Mensagem = "Arquivos :" + arquivoEnviado + ", com sucesso.";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Mensagem = "Erro :" + ex.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ Mvc_Upload/Mvc_Upload/Models/Remessa.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Mvc_Upload.Models
+{
+    public class Remessa
+    {
+        public IEnumerable<HttpPostedFileBase> Arquivos { get; set; }
+    }
+}
